6c52231a6d1b1275233222f59b50e8f82440a39d
bangzi1001 <36911788+bangzi1001@users.noreply.github.com>
2020-09-30 20:55:37 +0800

    Update signedtransactions.py

--- bitsharesbase/signedtransactions.py
+++ bitsharesbase/signedtransactions.py
@@ -18,5 +18,5 @@ class Signed_Transaction(GrapheneSigned_Transaction):
     """
 
     known_chains = known_chains
-    default_prefix = "BTS"
+    default_prefix = "NBS"
     operation_klass = Operation
